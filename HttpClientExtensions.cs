7d965cbbb1d72793602f09a08a7de7902e277d26
kerico <augjec@gmail.com>
2022-07-09 18:02:37 +0300

    moved twitch integration to single project

---
+++ HttpClientExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using TwitchIntegration.Model;
+
+namespace TwitchIntegration
+{
+    internal static class HttpClientExtensions
+    {
+        public static HttpRequestMessage AddStringContent(this HttpRequestMessage message, string content)
+        {
+            message.Content = new StringContent(content);
+            return message;
+        }
+        public static HttpRequestMessage AddStringContent(this HttpRequestMessage message, object content, string mediaType)
+        {
+            message.Content = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, mediaType);
+            return message;
+        }
+        public static HttpRequestMessage AddAuthenticationHeader(this HttpRequestMessage message, TwitchAccessToken token)
+        {
+            message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+            return message;
+        }
+        public static HttpRequestMessage AddClientIdHeader(this HttpRequestMessage message, string clientId)
+        {
+            message.Headers.Add("Client-Id", clientId);
+            return message;
+        }
+        public static async Task<T?> ReadAsAsync<T>(this HttpContent httpContent)
+        {
+            var stringContent = await httpContent.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<T>(stringContent ?? throw new Exception("No string content to read")) ?? default;
+        }
+    }
+}
---
+++ TwitchClient.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using TwitchIntegration.Model;
+
+namespace TwitchIntegration
+{
+    public class TwitchClient
+    {
+        private TwitchAccessToken? _accessToken;
+        private DateTime? _accessTokenExpiration;
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+        private readonly string _grantType;
+        private readonly HttpClient _httpClient = new()
+        {
+            BaseAddress = new Uri("https://api.twitch.tv/helix/")
+        };
+        private TwitchAccessToken AccessToken
+        {
+            get
+            {
+                GetAccessToken();
+
+                return _accessToken ?? throw new Exception("Failed to get access token");
+            }
+        }
+        public TwitchClient(string clientId, string clientSecret)
+        {
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _grantType = "client_credentials";
+        }
+        public async Task<TwitchStreamInfo[]> GetStreamsByLogin(params string[] streamLogins)
+        {
+            //var request = GetHttpRequestMessage("streams", HttpMethod.Get);
+            var request = GetHttpRequestMessage(AddParameters("streams", "user_login", streamLogins), HttpMethod.Get);
+            var resposne = await _httpClient.SendAsync(request);
+            resposne.EnsureSuccessStatusCode();
+            var content = await resposne.Content.ReadAsAsync<TwitchResponse<TwitchStreamInfo>>();
+            return content?.Values ?? Array.Empty<TwitchStreamInfo>();
+        }
+        public async Task<TwitchUserInfo[]> GetUsersByLogin(params string[] streamLogins)
+        {
+            //var request = GetHttpRequestMessage("streams", HttpMethod.Get);
+            var request = GetHttpRequestMessage(AddParameters("users", "login", streamLogins), HttpMethod.Get);
+            var resposne = await _httpClient.SendAsync(request);
+            resposne.EnsureSuccessStatusCode();
+            var content = await resposne.Content.ReadAsAsync<TwitchResponse<TwitchUserInfo>>();
+            return content?.Values ?? Array.Empty<TwitchUserInfo>();
+        }
+
+        private static string AddParameters(string originalPath,string parametername, string[] logins)
+        {
+            var result = originalPath;
+            for (int i = 0; i < logins.Length; i++)
+            {
+                result = QueryHelpers.AddQueryString(result, parametername, logins[i].ToLower());
+            }
+            return result;
+        }
+        private void GetAccessToken()
+        {
+            if (_accessTokenExpiration.HasValue && _accessTokenExpiration < DateTime.Now)
+                return;
+
+            var requestContent = new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                        { "client_id",  _clientId ?? throw new Exception("ClientID not provided")},
+                        { "client_secret", _clientSecret ?? throw new Exception("Client Secret not provided")},
+                        { "grant_type", _grantType },
+
+                    });
+
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://id.twitch.tv/"),
+                Timeout =new TimeSpan(0,0,30)
+            };
+
+            var response = httpClient.PostAsync("oauth2/token", requestContent).GetAwaiter().GetResult();
+            var accessToken = JsonConvert.DeserializeObject<TwitchAccessToken>(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+
+            _accessToken = accessToken;
+            _accessTokenExpiration = DateTime.Now.AddSeconds(accessToken?.ExpiresIn ?? 0);
+        }
+        private HttpRequestMessage GetHttpRequestMessage(string path, HttpMethod method) =>
+     new HttpRequestMessage { RequestUri = new Uri(_httpClient?.BaseAddress ?? throw new Exception("Http client not initialised"), path), Method = method }
+     .AddAuthenticationHeader(AccessToken).AddClientIdHeader(_clientId);
+    }
+}
